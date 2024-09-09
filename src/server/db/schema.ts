61be6da6f2cbcf201c635c366b74cd443dc6d0fe
Michael Henke <henke.mike@gmail.com>
2024-09-09 13:38:37 -0500

    updated table schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,18 +19,19 @@ import {
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
 export const posts = createTable(
-  "post",
+  "images",
   {
     id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
+    name: varchar("name", { length: 256 }).notNull(),
+    url: varchar("url", { length: 1024 }).notNull(),
     createdAt: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
