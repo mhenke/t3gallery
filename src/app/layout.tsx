ea0813ee39f50567cb501b71107037d9b22891d2
Michael Henke <henke.mike@gmail.com>
2024-09-16 15:46:45 -0500

    error handling done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import "@uploadthing/react/styles.css";
-import { type Metadata } from "next";
-import { Inter } from "next/font/google";
-import { extractRouterConfig } from "uploadthing/server";
-import "~/styles/globals.css";
-import { TopNav } from "./_components/topnav";
-import { ourFileRouter } from "./api/uploadthing/core";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import { Metadata } from "next";
+import NextSSRPlugin from "next-ssr-plugin";
 
 export const metadata: Metadata = {
   title: "T3 Gallery",
@@ -19,24 +8,28 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+interface RootLayoutProps {
+  readonly children: React.ReactNode;
+  readonly modal?: React.ReactNode;
+}
+
+export default function RootLayout(props: RootLayoutProps) {
+  const { children, modal } = props;
+
   return (
     <ClerkProvider>
       <html lang="en">
         <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(ourFileRouter)}
+        /**
+         * The `extractRouterConfig` will extract **only** the route configs
+         * from the router to prevent additional information from being
+         * leaked to the client. The data passed to the client is the same
+         * as if you were to fetch `/api/uploadthing` directly.
+         */
         />
-        <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <TopNav />
+        <body>
           {children}
+          {modal}
         </body>
       </html>
     </ClerkProvider>
